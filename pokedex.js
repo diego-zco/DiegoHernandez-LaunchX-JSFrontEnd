768610dcb45f69279a43e38f5e2e846ab3a8c09c
Diego Hernandez <diego.zco@gmail.com>
2022-03-28 00:01:30 -0600

    addapting top margin and adding first js function

--- pokedex.js
+++ pokedex.js
@@ -1,1 +1,28 @@
-console.log("Testing file");
+const consultarPokemon = () => {
+    let inputBuscar = document.getElementById("inputBuscar").value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${inputBuscar}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            document.warning("Pokemon no encontrado");
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg, "poke-front-image");
+            console.log(pokeImg);
+            pokeImg = data.sprites.back_default;
+            pokeImage(pokeImg, "poke-back-image");
+            console.log(pokeImg);
+        }
+    });
+}
+
+
+const pokeImage = (url, id) => {
+    const pokeImage = document.getElementById(id);
+    pokeImage.src = url;
+}
